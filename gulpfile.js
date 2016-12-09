604c4552e15ddc5e15ca3135d264d77681a8b1c3
leandrow <leandrow01s@gmail.com>
2016-12-09 01:50:25 -0300

    Lost grid added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 // Gulp Tasks
-var elixir = require('laravel-elixir');
+var elixir				= require('laravel-elixir'),
+		autoprefixer	= require('autoprefixer'),
+		postStylus		= require('poststylus'),
+		lost					= require('lost'),
+		mqpacker			= require('css-mqpacker');
 
 elixir.config.assetsPath = '';
 elixir.config.publicPath = 'dist';
@@ -12,8 +16,16 @@ elixir(function(mix) {
 	// Js
 	mix.scripts('*.js', 'dist/leal.min.js');
 
-	// CSS
-	mix.stylus('config.styl', 'dist/leal.min.css');
+	// Stylus config
+	var config = [
+		lost(),
+		mqpacker(),
+		autoprefixer()
+  ];
+	mix.stylus('config.styl', 'dist/leal.min.css', {
+		use: [postStylus(config)]
+	});
+
 	mix.copy('fonts', 'dist/fonts');
 
 	// Browser Sync
