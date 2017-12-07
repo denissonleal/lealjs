0e34adfe068e8a494e330e15abe91de27cfd3f74
Denisson Leal <denissonleal@Denissons-MacBook-Pro.local>
2017-12-07 16:35:23 -0300

    remove css e fonts do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,10 @@
 // Gulp Tasks
-var elixir				= require('laravel-elixir'),
-		autoprefixer	= require('autoprefixer'),
-		postStylus		= require('poststylus'),
-		lost					= require('lost'),
-		mqpacker			= require('css-mqpacker');
+var elixir				= require('laravel-elixir');
 
 elixir.config.assetsPath = '';
 elixir.config.publicPath = 'dist';
 
-// Extensions
-require('laravel-elixir-stylus')
-
-
 elixir(function(mix) {
 	// Js
 	mix.scripts('*.js', 'dist/leal.min.js');
-
-	// Stylus config
-	var config = [
-		lost(),
-		mqpacker(),
-		autoprefixer()
-  ];
-	mix.stylus('config.styl', 'dist/leal.min.css', {
-		use: [postStylus(config)]
-	});
-
-	mix.copy('fonts', 'dist/fonts');
-
-	// Browser Sync
-	mix.browserSync({
-		proxy: 'localhost:8000'
-	});
 });
